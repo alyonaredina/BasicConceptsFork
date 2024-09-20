a5cb708377f1fa53407dea4a5f9628252639c81c
Alyona Redina <medvedeva_64@mail.ru>
2024-09-20 22:31:12 +0600

    solving the first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
 "use strict"
+
 function solveEquation(a, b, c) {
+  //a*(x*x)+b*x+c=0
   let arr = [];
+  let discriminant = b * b - 4 * a * c;
+  if (discriminant < 0) {
+    console.log ('Уравнение не возвращает корней '); 
+
+  } else if (discriminant ===0) {
+    arr[0] = -b / (2 * a)
+    console.log ('Уравнение возвращает один корень ' + arr[0]);
+
+  } else { 
+    arr[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
+    console.log ('Уравнение возвращает два корня ' + arr[0] + ',' + arr[1]);
+  }
   
   return arr;
 }
